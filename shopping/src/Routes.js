dabcd119386508cfede507eac92961acfc330546
Capril1042 <Caprilewis1042@gmail.com>
2019-03-14 11:41:34 -0400

    Adds basic Routing

---
+++ shopping/src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Switch, Route } from 'react-router-dom';
+
+import LandingPage from './components/LandingPage.js';
+import SignIn from './components/Login/SignIn.js';
+import SignUp from './components/Login/SignUp.js';
+import Dashboard from './components/Dashboard.js';
+
+const Routes = () => {
+    return (
+        <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/sign-in" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
+        <Route path="/dashboard" component={Dashboard} /> 
+        </Switch>
+    );
+};
+
+export default Routes;
